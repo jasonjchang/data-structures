4b024c81100f88b7a9f0efca5d5034f2c12f6138
jasonjchang <hrjasonchang@gmail.com>
2015-02-06 21:27:16 -0800

    Finished Sets

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,29 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(this._storage === undefined){
+    return this._storage = [item];
+  } else {
+    return this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item){
+  for(var i=0; i < this._storage.length; i++){
+    if(this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  for(var i=0; i < this._storage.length; i++){
+    if(this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
+  return item;
 };
 
 /*
